20df66c24c5b4488d4046fce8feee75d56670d8b
Manuel Adeliño Consuegra <madelino@ucm.es>
2020-05-17 14:10:06 +0200

    Creado el script Pickable

---
+++ Assets/_Scripts/Pickable.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickable : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.GetComponent<PlayerController>() != null)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
